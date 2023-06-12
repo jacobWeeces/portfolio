ba767fab98b5f257e03a53633403a555e2e8614a
jWeeces <Jacobweecesdev@gmail.com>
2023-06-11 17:32:12 -0700

    Finished importing components. Let's go!

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,10 @@ const App = () => {
           <Tech />
           <Works />
           <Feedbacks />
-          <div className="relative z-0"></div>
+          <div className="relative z-0">
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
       </BrowserRouter>
   )
