79f2c7877c6ea268f0e380e2690e10a9c376aaa8
jWeeces <Jacobweecesdev@gmail.com>
2023-06-11 17:31:26 -0700

    Importing and utilizing components for structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from  './components';
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const App = () => {
   return (
-      <div>
-        <h1>Web Dev</h1>
-      </div>
+      <BrowserRouter>
+        <div className="relative z-0 bg-primary">
+          <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+            <Navbar />
+            <Hero />
+          </div>
+          <About />
+          <Experience />
+          <Tech />
+          <Works />
+          <Feedbacks />
+          <div className="relative z-0"></div>
+        </div>
+      </BrowserRouter>
   )
 }
 
