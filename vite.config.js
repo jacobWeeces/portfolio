1f9f40073b90a1ac6239230bda8647b7327de397
jWeeces <Jacobweecesdev@gmail.com>
2023-06-15 16:45:15 -0700

    More fixes. Yipee!

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/React_Portfolio/',
-  "scripts": {
-    "build": "vite build",
-    "preview": "vite preview"
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
   }
+
+  if (command !== 'serve') {
+    config.base = '/react-vite-gh-pages/'
+  }
+
+  return config
 })
