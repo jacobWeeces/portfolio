d00f6ac3b9fd28ec0721a6628c14037f37edf396
jWeeces <Jacobweecesdev@gmail.com>
2023-06-15 16:21:02 -0700

    Fixing gh-pages issues

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/React_Portfolio/',
+  "scripts": {
+    "build": "vite build",
+    "preview": "vite preview"
+  }
 })
