12ceb162b3181991395d309721bf5fe1b92f66a5
jWeeces <Jacobweecesdev@gmail.com>
2023-06-15 16:54:02 -0700

    Fixed a silly syntax error

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,12 +5,10 @@ import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-
-  const { scene } = useGLTF('./desktop_pc/scene.gltf');
-  
+  const { scene } = useGLTF('/desktop_pc/scene.gltf');
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor={black} />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       <pointLight intensity={1} />
       <primitive object={scene.children[0]} />
     </mesh>
